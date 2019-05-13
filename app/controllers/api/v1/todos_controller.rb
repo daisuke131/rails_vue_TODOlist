4d24e8cab7a881cfbbc479258540fbd282f3a8b4
daisuke <dada.00131@google.com>
2019-05-13 17:07:34 +0900

    add todos_controller

---
+++ app/controllers/api/v1/todos_controller.rb
@@ -0,0 +1,27 @@
+class Api::V1::TodosController < ApplicationController
+  before_action :set_todo, only: [:update, :destroy]
+  def index
+    render json: Todo.all.order("created_at DESC")
+  end
+
+  def create
+    @todo = Todo.create!(todo_params)
+    render json: @todo
+  end
+
+  def update
+    @todo.update!(todo_params)
+  end
+
+  def destroy
+    @todo.destroy!
+  end
+
+  private
+    def set_todo
+      @todo = Todo.find(params[:id])
+    end
+    def todo_params
+      params.require(:todo).permit(:body, :done)
+    end
+end
