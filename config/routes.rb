71e4f2274df7305c4fee62b6cb954e0d49eba16c
daisuke <dada.00131@google.com>
2019-05-13 17:06:24 +0900

    fix route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   root 'home#index'
+  namespace :api, { format: "json" } do
+    namespace :v1 do
+      resources :todos, :only => [:index, :create, :update, :destroy]
+    end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
